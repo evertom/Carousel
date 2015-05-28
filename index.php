04f9b01301daf33b07ee72286428727587bbe978
Everton J Paula <everton.projetos@gmail.com>
2015-05-28 19:34:59 -0300

    Carousel

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Carousel</title>
+        <link rel="stylesheet" href="font-awesome-4.3.0/css/font-awesome.min.css"/>
+        <script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+        <script type="text/javascript" src="js/jquery-ui.min.js"></script>
+        <script type="text/javascript" src="js/carousel.1.0.min.js"></script>
+        <script type="text/javascript">
+            $(document).ready(function(){
+                $('.carousel').Carousel();
+            });
+        </script>
+        
+    </head>
+    <body>
+        <div class='carousel' data-height='500px' data-width='900px'>
+            <img src="img/1.jpeg" />
+            <img src="img/2.jpg" />
+            <img src="img/3.jpg" />
+            <img src="img/4.jpg" />
+            <img src="img/5.jpg" />
+            <img src="img/6.jpg" />
+        </div>
+    </body>
+</html>
